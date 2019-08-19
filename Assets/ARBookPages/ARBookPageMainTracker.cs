61885b63306ad4692b784eb79800e890a2290508
enersesian <en6@njit.edu>
2019-08-18 20:31:48 -0400

    Initial commit of an AR textbook version of the binary math l

---
+++ Assets/ARBookPages/ARBookPageMainTracker.cs
@@ -0,0 +1,43 @@
+﻿namespace GoogleARCore.Examples.AugmentedImage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using GoogleARCoreInternal;
+    using UnityEngine;
+
+    public class ARBookPageMainTracker : MonoBehaviour
+    {
+        private bool[] interfaceBits = new bool[3];
+        private ARBookPageVisualizer visualizer;
+        private bool coolDownTime = true;
+
+        // Use this for initialization
+        void Start()
+        {
+            visualizer = GetComponent<ARBookPageVisualizer>();
+            for(int i = 0; i < 3; i++)
+            {
+                interfaceBits[i] = true;
+            }
+        }
+
+        public void SetInterface(int interfaceBit)
+        {
+            if(coolDownTime)
+            {
+                coolDownTime = false;
+                interfaceBits[interfaceBit] = !interfaceBits[interfaceBit];
+                //visualizer.ARBookPageElements[visualizer.thisInterfaceElement].transform.GetChild(interfaceBit).gameObject.SetActive(interfaceBits[interfaceBit]);
+                transform.GetChild(0).GetChild(interfaceBit).gameObject.SetActive(interfaceBits[interfaceBit]);
+                Invoke("ResetCoolDownTimer", 2f);
+            }
+        }
+
+        private void ResetCoolDownTimer()
+        {
+            coolDownTime = true;
+        }
+    }
+}
---
+++ Assets/ARBookPages/ARBookPageSecondaryTracker.cs
@@ -0,0 +1,54 @@
+﻿namespace GoogleARCore.Examples.AugmentedImage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using GoogleARCoreInternal;
+    using UnityEngine;
+
+    public class ARBookPageSecondaryTracker : MonoBehaviour
+    {
+        private ARBookPageVisualizer visualizer;
+        private ARBookPageMainTracker mainTracker;
+        private float timeSinceFullTrackingMethod;
+        private bool hasBeenFullTracked;
+
+        private void Start()
+        {
+            visualizer = GetComponent<ARBookPageVisualizer>();
+            mainTracker = visualizer.GetMainTracker();
+        }
+
+        public void Update()
+        {
+            if (visualizer.Image == null || visualizer.Image.TrackingState != TrackingState.Tracking)
+            {
+                foreach (var element in visualizer.ARBookPageElements) element.SetActive(false);
+                return;
+            }
+
+            if (visualizer.Image.TrackingMethod == AugmentedImageTrackingMethod.FullTracking)
+            {
+                hasBeenFullTracked = true;
+            }
+
+            if (visualizer.Image.TrackingMethod == AugmentedImageTrackingMethod.LastKnownPose && hasBeenFullTracked)
+            {
+                timeSinceFullTrackingMethod += Time.deltaTime;
+                if (timeSinceFullTrackingMethod > 1f)
+                {
+                    visualizer.ARBookPageElements[visualizer.thisInterfaceElement].SetActive(false);
+                    mainTracker = visualizer.GetMainTracker(); //secondary tracker may be created before main tracker and cant set this in start method
+                    mainTracker.SetInterface(visualizer.thisInterfaceElement - 1);
+                    timeSinceFullTrackingMethod = 0f;
+                }
+            }
+            else
+            {
+                visualizer.ARBookPageElements[visualizer.thisInterfaceElement].SetActive(true);
+                timeSinceFullTrackingMethod = 0f;
+            }
+        }
+    }
+}
