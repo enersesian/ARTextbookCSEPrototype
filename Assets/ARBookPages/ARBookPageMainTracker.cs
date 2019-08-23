a6c1ff592acad81b69814dfb85248e4140189b0e
Eric Nersesian <eric.nersesian@njit.edu>
2019-08-23 15:58:49 -0400

    Added temporary station models to cycle through with touch event

--- Assets/ARBookPages/ARBookPageMainTracker.cs
+++ Assets/ARBookPages/ARBookPageMainTracker.cs
@@ -12,6 +12,7 @@
         private bool[] interfaceBits = new bool[3];
         private ARBookPageVisualizer visualizer;
         private bool coolDownTime = true;
+        private int stationCounter;
 
         // Use this for initialization
         void Start()
@@ -23,14 +24,43 @@
             }
         }
 
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                switch(stationCounter)
+                {
+                    case 0:
+                        transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
+                        transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+                        stationCounter = 1;
+                        break;
+                    case 1:
+                        transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
+                        transform.GetChild(0).GetChild(2).gameObject.SetActive(true);
+                        stationCounter = 2;
+                        break;
+                    case 2:
+                        transform.GetChild(0).GetChild(2).gameObject.SetActive(false);
+                        transform.GetChild(0).GetChild(3).gameObject.SetActive(true);
+                        stationCounter = 3;
+                        break;
+                    case 3:
+                        transform.GetChild(0).GetChild(3).gameObject.SetActive(false);
+                        transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
+                        stationCounter = 0;
+                        break;
+                }
+            }
+        }
+
         public void SetInterface(int interfaceBit)
         {
             if(coolDownTime)
             {
                 coolDownTime = false;
                 interfaceBits[interfaceBit] = !interfaceBits[interfaceBit];
-                //visualizer.ARBookPageElements[visualizer.thisInterfaceElement].transform.GetChild(interfaceBit).gameObject.SetActive(interfaceBits[interfaceBit]);
-                transform.GetChild(0).GetChild(interfaceBit).gameObject.SetActive(interfaceBits[interfaceBit]);
+                //transform.GetChild(0).GetChild(interfaceBit).gameObject.SetActive(interfaceBits[interfaceBit]);
                 Invoke("ResetCoolDownTimer", 2f);
             }
         }
